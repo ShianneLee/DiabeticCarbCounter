584aa12bcb504e971c3b38733a76235a862cd27a
Shianne <shiannelee@live.ca>
2019-05-28 22:00:19 -0400

    Initial commit with known activities and views

---
+++ app/src/main/java/com/shianne/java/diabeticcarbcounter/CalculationDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.shianne.java.diabeticcarbcounter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+// Calc details
+class CalculationDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculation_details)
+    }
+}
---
+++ app/src/main/java/com/shianne/java/diabeticcarbcounter/CurrentInsulinActivity.kt
@@ -0,0 +1,13 @@
+package com.shianne.java.diabeticcarbcounter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+// Current Insulin
+class CurrentInsulinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_insulin)
+    }
+}
